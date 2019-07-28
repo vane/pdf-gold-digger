9b0574777ffffbb8cb633e3c0c473d2127c41d5e
Michal Szczepanski <michal@vane.pl>
2019-07-28 17:34:16 +0200

    Update demo.sh with xml output and extract font

--- demo.sh
+++ demo.sh
@@ -2,4 +2,5 @@
 mkdir -p out
 wget -O out/test.pdf -nc https://github.com/mozilla/pdf.js/raw/master/web/compressed.tracemonkey-pldi-09.pdf
 node gd.js  -i './out/test.pdf' -f json
-node gd.js  -i './out/test.pdf' -f text
+node gd.js  -i './out/test.pdf' -f xml
+node gd.js  -i './out/test.pdf' -f text -t
