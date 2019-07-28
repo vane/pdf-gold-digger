2b1cf8c08f4fab44d203f3fd7dd6f3943617b127
Michal Szczepanski <michal@vane.pl>
2019-07-28 22:08:22 +0200

    Add html output to demo.sh

--- demo.sh
+++ demo.sh
@@ -3,4 +3,5 @@ mkdir -p out
 wget -O out/test.pdf -nc https://github.com/mozilla/pdf.js/raw/master/web/compressed.tracemonkey-pldi-09.pdf
 node gd.js  -i './out/test.pdf' -f json
 node gd.js  -i './out/test.pdf' -f xml
+node gd.js  -i './out/test.pdf' -f html
 node gd.js  -i './out/test.pdf' -f text -t
